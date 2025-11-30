1d1211b3af6dd2078bd8de687d884041b24a103c
Vishnu19091 <vishnusah368@gmail.com>
2025-11-30 17:19:42 +0530

    auth/signuppage -> registers a new user

--- src/app/auth/signup/page.js
+++ src/app/auth/signup/page.js
@@ -1,20 +1,68 @@
+"use client";
 import Link from "next/link";
 import styles from "./signup.module.css";
+import { Account, ID } from "appwrite";
+import { client } from "@/app/_lib/appwrite";
+import { useState } from "react";
+import { redirect } from "next/navigation";
 
 export default function Page() {
+  const account = new Account(client);
+
+  const [userName, setUserName] = useState(null | "");
+  const [email, setEmail] = useState(null | "");
+  const [passwd, setPasswd] = useState(null | "");
+
+  async function onFormSubmit(e) {
+    e.preventDefault();
+
+    if (
+      userName &&
+      email &&
+      passwd &&
+      userName.length > 4 &&
+      email.length > 5 &&
+      passwd.length > 8
+    ) {
+      try {
+        const user = await account.create({
+          userId: ID.unique(),
+          email: email,
+          password: passwd,
+          name: userName,
+        });
+      } catch (error) {
+        console.error("Unable to register a new user!", error);
+      }
+      redirect("/auth/signin");
+    }
+  }
+
   return (
     <div className={styles.card}>
       <div className={styles.title}>Sign Up</div>
 
       <p className={styles.subtitle}>Please enter your details to sign up</p>
 
-      <form className={styles.form}>
-        <div className="grid grid-cols-2 gap-2">
-          <input placeholder="John *" type="text" />
-          <input placeholder="Doe *" type="text" />
-        </div>
-        <input placeholder="jon.doe@gmail.com *" type="email" />
-        <input placeholder="Enter a password *" type="password" />
+      <form className={styles.form} onSubmit={onFormSubmit}>
+        <input
+          placeholder="John Doe *"
+          type="text"
+          onChange={(e) => setUserName(e.target.value)}
+        />
+
+        <input
+          placeholder="jon.doe@gmail.com *"
+          type="email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <input
+          placeholder="Enter a password *"
+          type="password"
+          onChange={(e) => setPasswd(e.target.value)}
+        />
+
         <button type="submit" className={styles.primaryBtn}>
           Sign up
         </button>
