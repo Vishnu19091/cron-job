ae96cf54e7967b55c70b5aec1876640f10d7d1ab
Vishnu19091 <vishnusah368@gmail.com>
2025-12-14 12:38:44 +0530

    Fix Page redirection

--- src/app/(public)/auth/page.js
+++ src/app/(public)/auth/page.js
@@ -1,13 +1,6 @@
 "use client";
 import { redirect } from "next/navigation";
-import { useGetUser } from "../../_hooks/useGetUser";
 
 export default function Page() {
-  const { user } = useGetUser();
-
-  if (!user) {
-    redirect("/auth/signin");
-  } else if (user) {
-    redirect("/dashboard");
-  }
+  redirect("/auth/signin");
 }
